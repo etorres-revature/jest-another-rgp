ce7e2844c727a47505fb81168e3885345fe1e743
Eric D. Torres <etorresnotary@gmail.com>
2021-09-30 00:36:33 -0500

    Enemy object created

--- __test__/Enemy.test.js
+++ __test__/Enemy.test.js
@@ -1,16 +1,16 @@
 const { expect } = require("@jest/globals");
 const Enemy = require("../lib/Enemy");
-const Potion = require("../lib/Potion")
+const Potion = require("../lib/Potion");
 
 jest.mock("../lib/Potion");
 
 test("creates an enemy object", () => {
-    const enemy = new Enemy("goblin, sword");
+  const enemy = new Enemy("goblin", "sword");
 
-    expect(enemy.name).toBe("goblin")
-    expect(enemy.weapon).toBe("sword")
-    expect(enemy.health).toBe(expect.any(Number))
-    expect(enemy.strength).toBe(expect.any(Number))
-    expect(enemy.agility).toBe(expect.any(Number))
-    expect(enemy.potion).toBe(expect.any(Number))
-})
+  expect(enemy.name).toBe("goblin");
+  expect(enemy.weapon).toBe("sword");
+  expect(enemy.health).toEqual(expect.any(Number));
+  expect(enemy.strength).toEqual(expect.any(Number));
+  expect(enemy.agility).toEqual(expect.any(Number));
+  expect(enemy.potion).toEqual(expect.any(Object));
+});
